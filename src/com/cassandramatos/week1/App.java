5418b7246ffbad6a264ae6b14ddddc31ed8f641c
Cassandra Matos <cassandra@matosmendez.com>
2021-05-19 20:17:18 -0700

    hello app

---
+++ src/com/cassandramatos/week1/App.java
@@ -0,0 +1,12 @@
+package com.cassandramatos.week1;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+
+	}
+
+}
